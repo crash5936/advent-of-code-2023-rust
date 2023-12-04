1537013a179ce4bab4569f124500cd7a9c5657c8
Jakub Smetana <jakubsmetana@gmail.com>
2023-12-04 18:28:34 +0100

    consolidate pattern branches

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,11 @@ fn main() {
     let args = Cli::parse();
 
     match args.day.as_str() {
-        "1" => days::day1::part1(),
-        "1-1" => days::day1::part1(),
+        "1" | "1-1" => days::day1::part1(),
         "1-2" => days::day1::part2(),
-        "2" => days::day2::part1(),
-        "2-1" => days::day2::part1(),
+        "2" | "2-1" => days::day2::part1(),
         "2-2" => days::day2::part2(),
+        "3" | "3-1" => days::day3::part1(),
         _ => println!("Day not implemented"),
     }
 }
